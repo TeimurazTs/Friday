0e018e37e4e51f27b75b80ff28a8d203825a6a32
Teimuraz Tsuladze <temotsuladze99@gmail.com>
2023-06-23 14:28:17 +0400

    move to standalone components and create page structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @Component({
+  standalone: true,
+  imports: [DashboardComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'Friday';
-}
+export class AppComponent {}
---
+++ src/app/shared/components/task/task.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css']
+})
+export class TaskComponent {
+
+}
